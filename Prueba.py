c340984b2a4d0daeea3e880d57d005e48710361e
Kanjore <154010265+Kanjore@users.noreply.github.com>
2024-02-03 15:16:07 -0500

    Add files via upload

---
+++ Prueba.py
@@ -0,0 +1,15 @@
+import log_d
+v1 = 0
+v2 = 0
+salida1 = log_d.AND(v1, v2)
+salida2 = log_d.XNOR(v1, v2)
+salida3 = log_d.OR()
+dato1 = "prueba1"
+dato2 = "prueba2"
+print(salida3)
+Mult = log_d.MUX(1,0)
+print(Mult)
+
+Flipflop = log_d.FLIPFLOP(0, 1)
+print(Flipflop)
+
